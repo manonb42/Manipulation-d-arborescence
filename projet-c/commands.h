be352789cf9dbe1b165ac5ed25fd25ad848391ad
manonb42 <95853515+manonb42@users.noreply.github.com>
2024-02-19 18:39:54 +0100

    Delete projet-c directory

--- projet-c/commands.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMANDS_H_
-#define COMMANDS_H_
-
-#include "arbre.h"
-
-noeud *exec_command(noeud *wd, int argc, char **argv);
-
-#endif // COMMANDS_H_
--- projet-c/parse.h
+++
@@ -1,7 +0,0 @@
-#ifndef PARSE_H
-#define PARSE_H
-#include <stdio.h>
-
-char **read_command(FILE *file);
-
-#endif
